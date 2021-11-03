e3ae20319db0cfdcfc796f5b0c87e9494a5d220e
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-11-03 12:58:11 +0530

    Updated Splash screen, Login authentication

---
+++ app/src/main/java/com/example/foodvisor/LoginActivity.kt
@@ -0,0 +1,111 @@
+package com.example.foodvisor
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private val TAG = "GoogleLogin"
+    private val RC_SIGN_IN = 99
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+
+        val user: FirebaseUser? = mAuth.currentUser
+        if (user != null) {
+            val intent = Intent(this, ProfileCreation::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+        loginWithGoogle()
+        mAuth = FirebaseAuth.getInstance()
+
+        btnGoogleLogin.setOnClickListener {
+            signIn()
+        }
+
+        btnFacebookLogin.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun loginWithGoogle() {
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this,
+                OnCompleteListener<AuthResult?> { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user: FirebaseUser? = mAuth.currentUser
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT)
+                            .show()
+                        val intent = Intent(this, ProfileCreation::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                    }
+                })
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodvisor/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.foodvisor
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var timer: Timer
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000)
+
+    }
+}
