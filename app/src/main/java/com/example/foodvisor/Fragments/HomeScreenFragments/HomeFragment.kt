640db383305f91b67d5e99c10ad1c9e2fc441348
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-11-11 14:15:17 +0530

    updated Home fragment

--- app/src/main/java/com/example/foodvisor/Fragments/HomeScreenFragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodvisor/Fragments/HomeScreenFragments/HomeFragment.kt
@@ -1,9 +1,51 @@
 package com.example.foodvisor.Fragments.HomeScreenFragments
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.foodvisor.R
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val circularProgressBar = view.findViewById<CircularProgressBar>(R.id.circularProgressBar)
+        circularProgressBar.apply {
+            // Set Progress
+            progress = 65f
+            // or with animation
+            setProgressWithAnimation(65f, 1000) // =1s
+
+            // Set Progress Max
+            progressMax = 200f
+
+            // Set ProgressBar Color
+            progressBarColor = Color.BLACK
+            // or with gradient
+            progressBarColorStart = Color.GRAY
+            progressBarColorEnd = Color.BLUE
+            progressBarColorDirection = CircularProgressBar.GradientDirection.LEFT_TO_RIGHT
+
+            // Set background ProgressBar Color
+            backgroundProgressBarColor = Color.GRAY
+            // or with gradient
+            backgroundProgressBarColorStart = Color.WHITE
+            backgroundProgressBarColorEnd = Color.RED
+            backgroundProgressBarColorDirection =
+                CircularProgressBar.GradientDirection.LEFT_TO_RIGHT
+
+            // Set Width
+            progressBarWidth = 5f // in DP
+            backgroundProgressBarWidth = 10f // in DP
+
+            // Other
+            roundBorder = true
+            startAngle = 180f
+            progressDirection = CircularProgressBar.ProgressDirection.TO_LEFT
+        }
+    }
 }
